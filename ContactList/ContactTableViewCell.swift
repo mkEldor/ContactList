cf1e1c0c0c144ba552cb7d31729c94961f3e9440
Eldor Makkambaev <eldor@192.168.1.8>
2018-04-30 17:35:09 +0600

    release v1

---
+++ ContactList/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  ContactList
+//
+//  Created by Eldor Makkambaev on 30.04.2018.
+//  Copyright © 2018 Eldor Makkambaev. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
